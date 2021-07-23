48b9bd3f4e253d793f6674ac61ceb976f92dfc5e
Rex Yagami <40285992+rexyagami@users.noreply.github.com>
2021-07-23 12:41:21 +0530

    added floating text

---
+++ assets/js/slider-text.js
@@ -0,0 +1,9 @@
+// Getting the Paragraph from HTML
+var floating_text = document.getElementById("slider-text");
+var given_text;
+
+// Enter or Change your text here
+given_text = "ITR free for Senior Citizen Army Man";
+
+// Pushing Given Text to HTML
+floating_text.innerHTML = given_text;
