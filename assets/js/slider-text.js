663a7613019f0676a263e87cd62770ec2e942366
Ankit <officialankittriapthi09@gmail.com>
2022-10-04 11:52:58 +0530

    sliding header updated

--- assets/js/slider-text.js
+++ assets/js/slider-text.js
@@ -3,7 +3,7 @@ var floating_text = document.getElementById("slider-text");
 var given_text;
 
 // Enter or Change your text here
-given_text = "ITR free for Senior Citizen Army Man";
+given_text = "Compensation, penalties cannot be taxed: Govt||Delhi's GST collection in second quarter of FY23 dips by more than 6%||Rupee depreciates 38 paise to 81.78 against US dollar in early trade.";
 
 // Pushing Given Text to HTML
 floating_text.innerHTML = given_text;
