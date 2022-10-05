a3091754eda2e1cdf9da262d23f13d046957ac64
Ankit <officialankittriapthi09@gmail.com>
2022-10-05 12:13:17 +0530

    sliding header updated

--- assets/js/slider-text.js
+++ assets/js/slider-text.js
@@ -1,9 +1,13 @@
 // Getting the Paragraph from HTML
 var floating_text = document.getElementById("slider-text");
+var floating_text2 = document.getElementById("slider-text2");
 var given_text;
+var given_text2;
+
 
 // Enter or Change your text here
-given_text = "Compensation, penalties cannot be taxed: Govt||Delhi's GST collection in second quarter of FY23 dips by more than 6%||Rupee depreciates 38 paise to 81.78 against US dollar in early trade.";
-
+given_text = "Rupee depreciates 38 paise to 81.78 against US dollar in early trade. || Delhi's GST collection in second quarter of FY23 dips by more than 6%.";
+given_text2 = "GST collection crosses Rs 1.47 lakh crore mark in September. || Compensation, penalties cannot be taxed: Govt.";
 // Pushing Given Text to HTML
 floating_text.innerHTML = given_text;
+floating_text2.innerHTML = given_text2;
